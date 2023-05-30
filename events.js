baaa3119b51232dd92380de139fbd7a10fd3967b
Scott Parrish <slparrish@gmail.com>
2023-05-30 02:09:05 -0400

    messing around

---
+++ events.js
@@ -0,0 +1,7 @@
+
+function doSomething() {
+    console.log('Square clicked...');
+
+    document.getElementById('block').style.backgroundColor = 'crimson';
+    document.getElementById('block').style.borderRadius = '100%';
+}
